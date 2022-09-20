ff897ae25a584a9d49b19e852e2a5b1e6b33667e
16-53 <miha.otk@gmail.com>
2022-09-20 20:05:41 +0300

    Add timetable for today and tomorrow by command

--- main.py
+++ main.py
@@ -11,30 +11,56 @@ bot = TeleBot(token)
 
 @bot.message_handler(commands=["start", "help"])
 def start(message):
-    text = "Hey there! \n" "Write /next_lesson to find out what the next lesson is"
+    text = (
+        "Hey there! \n"
+        "Commands: \n"
+        "/next_lesson - to find out what the next lesson is \n"
+        "/tt_today - to get today's timetable \n"
+        "/tt_tomorrow - to get tomorrow's timetable"
+    )
     bot.send_message(message.chat.id, text, reply_markup=actions())
 
 
-@bot.message_handler(commands=["timetable_today"])
+@bot.message_handler(commands=["tt_today"])
 def tt_today(message):
     date_now = date.today()
     weekday = calendar.day_name[date_now.weekday()]
-    
+
     if weekday == "Sunday" or weekday == "Saturday":
-        bot.send_message(message.chat.id, "No lessons for today!")    
+        bot.send_message(message.chat.id, "No lessons for today!")
 
     else:
         time_table = timetable(weekday)
         text = ""
-        
+
         for i in range(len(time_table)):
-            text += "\n"+ (time_table[i][0] + " - " + time_table[i][1])
+            text += "\n" + (time_table[i][0] + " - " + time_table[i][1])
             for j in range(len(time_table[i][2])):
                 text += "\n" + time_table[i][2][j]
             text += "\n"
-                
+
         bot.send_message(message.chat.id, text)
-            
+
+
+@bot.message_handler(commands=["tt_tomorrow"])
+def tt_tomorrow(message):
+    date_tomorrow = date.today() + timedelta(days=1)
+    weekday = calendar.day_name[date_tomorrow.weekday()]
+
+    if weekday == "Sunday" or weekday == "Saturday":
+        bot.send_message(message.chat.id, "No lessons for tomorrow!")
+
+    else:
+        time_table = timetable(weekday)
+        text = ""
+
+        for i in range(len(time_table)):
+            text += "\n" + (time_table[i][0] + " - " + time_table[i][1])
+            for j in range(len(time_table[i][2])):
+                text += "\n" + time_table[i][2][j]
+            text += "\n"
+
+        bot.send_message(message.chat.id, text)
 
 
 @bot.message_handler(commands=["next_lesson"])
@@ -43,17 +69,16 @@ def lesson(message):
     hour = (datetime.utcnow() + timedelta(hours=3)).hour
     minute = (datetime.utcnow() + timedelta(hours=3)).minute
     weekday = calendar.day_name[date_now.weekday()]
-        
+
     if weekday == "Sunday" or weekday == "Saturday":
         bot.send_message(message.chat.id, "Have a nice weekend!")
-        
-    else:       
+
+    else:
         time_table = timetable(weekday)
         text = ""
 
         for i in range(len(time_table) - 1):
             start_lesson_2 = list(map(int, time_table[i + 1][0].split(":")))
-
 
             if hour < 7 or hour == 7 and minute <= 45:
                 text = time_table[i][0] + " - " + time_table[i][1]
@@ -61,17 +86,16 @@ def lesson(message):
                     text += "\n" + time_table[0][2][j]
                 bot.send_message(message.chat.id, text)
                 break
-            
-            elif (
-                hour < start_lesson_2[0]
-                or (hour == start_lesson_2[0] and minute <= start_lesson_2[1])
+
+            elif hour < start_lesson_2[0] or (
+                hour == start_lesson_2[0] and minute <= start_lesson_2[1]
             ):
-                text = time_table[i+1][0] + " - " + time_table[i+1][1]
-                for j in range(len(time_table[i+1][2])):
-                    text += "\n" + time_table[i+1][2][j] 
+                text = time_table[i + 1][0] + " - " + time_table[i + 1][1]
+                for j in range(len(time_table[i + 1][2])):
+                    text += "\n" + time_table[i + 1][2][j]
                 bot.send_message(message.chat.id, text)
                 break
-            
+
         if text == "":
             text = "Congratulations! No more lessons for today!"
             bot.send_message(message.chat.id, text)
@@ -79,11 +103,15 @@ def lesson(message):
 
 def actions():
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
-    keyboard.row_width = 2
+    keyboard.row_width = 3
+
     button_next_lesson = types.KeyboardButton("/next_lesson")
-    button_tt_today = types.KeyboardButton("/timetable_today")
-    keyboard.row(button_next_lesson, button_tt_today)
-    
+    button_tt_today = types.KeyboardButton("/tt_today")
+    button_tt_tomorrow = types.KeyboardButton("/tt_tomorrow")
+
+    keyboard.row(button_next_lesson, button_tt_today, button_tt_tomorrow)
+
     return keyboard
+
 
 bot.polling(none_stop=True, interval=0)
